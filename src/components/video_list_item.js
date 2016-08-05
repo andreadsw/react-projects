2a746282d6e8a43622aba2807051b1b212e0528f
andreadsw <andrea.dossantosw@gmail.com>
2016-08-05 16:04:44 +0100

    adding react boilerplate

--- src/components/video_list_item.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const VideoListitem = (props) => {
-  return <li>video</li>
-}
-
-export default VideoListitem
--- src/index.js
+++ src/index.js
@@ -1,42 +1,5 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import YTSearch from 'youtube-api-search'
+import App from './App'
 
-import SearchBar from './components/search_bar'
-import VideoList from './components/video_list'
-
-const API_KEY = 'AIzaSyDFk3YpPU1N2apbYQTknFr4IWXWSuS-gAk'
-
-
-
-// create a new component. This should produce some HTML
-// const nevers change it.
-// Create a class component because we need to use state and more advance react
-// features
-class App extends Component {
-  constructor (props) {
-    super(props)
-
-    // initial value. Create an object videos is a key of the object. Create an
-    //array is the value of the object.
-    this.state = { videos: [] }
-    // Demonstrate how the Youtube API works here. Then, term is surfbards to search
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos })
-      // this line is the same of  key: value this.setState({ videos: videos})
-    })
-  }
-  render () {
-    return (
-      <div>
-        <SearchBar />
-        <VideoList videos= {this.state.videos} />
-      </div>
-    )
-  }
-}
-
-
-// Take this component amd generated HTML and put it on the page (in the DOM)
-
-ReactDOM.render(<App />, document.getElementById('root')) // Create instances of App
+ReactDOM.render(<App />, document.getElementById('root'))
